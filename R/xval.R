11db0b1a38693d33f63dcc6e692b7ca5e22375c2
Xi Cheng <xicheng@ucdavis.edu>
2018-04-14 20:21:55 -0700

    add multlog option

--- R/xval.R
+++ R/xval.R
@@ -26,36 +26,66 @@ xvalPoly <- function(xy, maxDeg, maxInteractDeg = maxDeg, use = "lm",
                      yCol = NULL,printTimes=TRUE) 
 {
   if (!is.null(yCol)) xy <- moveY(xy,yCol)
+  
+  y <- xy[,ncol(xy)]
+  
+  if (pcaMethod) {
+    tmp <- system.time(
+      xy.pca <- prcomp(xy[,-ncol(xy)])
+    )
+    if (printTimes) cat('PCA time in xvalPoly: ',tmp,'\n')
+    pcNo = cumsum(xy.pca$sdev)/sum(xy.pca$sdev)
+    for (k in 1:length(pcNo)) {
+      if (pcNo[k] >= pcaPortion)
+        break
+    }
+    xdata <- xy.pca$x[,1:k, drop=FALSE]
+  } else {
+    xdata <- xy[,-ncol(xy), drop=FALSE]
+  }
+  
+  tmp <- system.time(
+    poly.xy <- getPoly(xdata, maxDeg, maxInteractDeg)
+  )
+  if (printTimes) cat('getPoly time in xvalPoly: ',tmp,'\n')
+  
+  xy <- cbind(poly.xy$xdata, y)
+  
   tmp <- splitData(xy,nHoldout)
   training <- tmp$trainSet
   testing <- tmp$testSet
+  train.y <- training[,ncol(training)]
+  test.y <- testing[,ncol(testing)]
+#  testIdx <- splitData(xy, nHoldout,TRUE)
   
-  tmp <- system.time(
-     {polyMatTrain <- 
-        getPoly(training[,-ncol(training)], maxDeg, maxInteractDeg);
-      polyMatTest <- 
-        getPoly(testing[,-ncol(testing)], maxDeg, maxInteractDeg)})
-  if (printTimes) cat('getPoly() time: ',tmp,'\n')
-
   acc <- NULL
   for (i in 1:maxDeg) {  # for each degree
     m <- ifelse(i > maxInteractDeg, maxInteractDeg, i)
     
-    endCol1 <- polyMatTrain$endCols[i]
-    polyMat1 <- polyMatTrain$xdata[,1:endCol1]
-    pol <- polyFit(training, i, m, use, pcaMethod, pcaPortion, glmMethod, 
-                   polyMat = polyMat1)
+    endCol <- poly.xy$endCols[i]
     
-    endCol2 <- polyMatTest$endCols[i]
-    polyMat2 <- polyMatTest$xdata[,1:endCol2]
-    pred <- predict(pol, testing[,-ncol(testing)], polyMat2)
+#    if (use == "glm" & glmMethod == "multlog" & length(unique(y)) > 2) {
+#      # multinomial logistic
+#      dat <- cbind(xy[,1:endCol], y)
+#      pol <- polyFit(dat, i, m, use, pcaMethod = FALSE, pcaPortion, glmMethod,
+#                     polyMat = dat, testIdx = testIdx)
+#      pred <- predict(pol, test1, test1, testIdx)
+#    } else {
+      train1 <- cbind(training[,1:endCol], train.y)
+      colnames(train1)[ncol(train1)] <- "y"
+      test1 <- testing[,1:endCol]
+      
+      pol <- polyFit(train1, i, m, use, pcaMethod = FALSE, pcaPortion, glmMethod,
+                     polyMat = train1)
+      pred <- predict(pol, test1, test1)
+#    }
     
     if (use == "lm") {
-      acc[i] <- mean(abs(pred - testing[,ncol(testing)]))
-    } else if (use == "glm")
-      acc[i] <- mean(pred == testing[,ncol(testing)]) # accuracy
-
-  }
+      acc[i] <- mean(abs(pred - test.y))
+    } else
+      acc[i] <- mean(pred == test.y) # accuracy
+    
+  } # for each degree
   return(acc)
 }
 
@@ -77,32 +107,33 @@ xvalPoly <- function(xy, maxDeg, maxInteractDeg = maxDeg, use = "lm",
 #' @export
 
 xvalNnet <- function(xy,size,linout, pcaMethod = FALSE,pcaPortion = 0.9,
-               scaleXMat = FALSE, nHoldout=10000, yCol = NULL)
+                     scaleXMat = FALSE, nHoldout=10000, yCol = NULL)
 {
-   require(nnet)
-   ncxy <- ncol(xy)
-
-   if (!is.null(yCol)) xy <- moveY(xy,yCol)
- 
-   if (scaleXMat) xy <- scaleX(xy)  # only Xs are scaled
-   tmp <- splitData(xy,nHoldout)
-   training <- tmp$trainSet
-   testingx <- tmp$testSet[,-ncxy]
-   testingy <- tmp$testSet[,ncxy]
-
-   yName <- names(xy)[ncol(xy)]
-   cmd <- paste0('nnout <- nnet(',yName,' ~ .,data=training,size=')
-   cmd <- paste0(cmd,size,',linout=',linout,')')
-   eval(parse(text=cmd))
-   preds <- predict(nnout,testingx)
-   trainingy <- training[,ncxy]
-   if (!is.factor(trainingy))  # regression case
-     return(mean(abs(preds - testingy)))
-   # classification case
-   preds <- apply(preds,1,which.max)  # column numbers
-   # convert to levels of Y
-   preds <- levels(trainingy)[preds]
-   return(mean(preds == testingy))
+  require(nnet)
+  ncxy <- ncol(xy)
+  
+  if (!is.null(yCol)) xy <- moveY(xy,yCol)
+  
+  if (scaleXMat) xy <- scaleX(xy)  # only Xs are scaled
+  tmp <- splitData(xy,nHoldout)
+  training <- tmp$trainSet
+  testingx <- tmp$testSet[,-ncxy]
+  testingy <- tmp$testSet[,ncxy]
+  
+  
+  yName <- names(xy)[ncol(xy)]
+  cmd <- paste0('nnout <- nnet(',yName,' ~ .,data=training,size=')
+  cmd <- paste0(cmd,size,',linout=',linout,')')
+  eval(parse(text=cmd))
+  preds <- predict(nnout,testingx)
+  trainingy <- training[,ncxy]
+  if (!is.factor(trainingy))  # regression case
+    return(mean(abs(preds - testingy)))
+  # classification case
+  preds <- apply(preds,1,which.max)  # column numbers
+  # convert to levels of Y
+  preds <- levels(trainingy)[preds]
+  return(mean(preds == testingy))
 }
 
 ##################################################################
@@ -127,35 +158,35 @@ xvalNnet <- function(xy,size,linout, pcaMethod = FALSE,pcaPortion = 0.9,
 #' @export
 
 xvalDnet <- function(x,y,hidden,output='"sigm"',numepochs=3,
-               pcaMethod = FALSE,pcaPortion = 0.9,
-               scaleXMat = TRUE, nHoldout=10000)
+                     pcaMethod = FALSE,pcaPortion = 0.9,
+                     scaleXMat = TRUE, nHoldout=10000)
 {
-   require(deepnet)
-
-   if (scaleXMat) x <- scale(x)
-
-   tmp <- splitData(x,nHoldout,idxsOnly=TRUE)
-   trainingx <- x[-tmp,]
-   testingx <- x[tmp,]
-   ym <- as.matrix(y)
-   trainingy <- ym[-tmp,]
-   testingy <- ym[tmp,]
-
-   cmd <- paste0('nnout <- nn.train(trainingx,trainingy,')
-   cmd <- paste0(cmd,'hidden=',hidden,',') 
-   cmd <- paste0(cmd,'output=',output,',') 
-   cmd <- paste0(cmd,'numepochs=',numepochs) 
-   cmd <- paste0(cmd,')')
-   eval(parse(text=cmd))
-   preds <- nn.predict(nnout,testingx)
-   if (ncol(ym) == 1)  # regression case
-      return(mean(abs(preds - testingy)))
-   # else
-   preds <- apply(preds,1,which.max)  # column numbers
-   trueY <- apply(testingy,1,function(rw) which(rw == 1))
-   # convert to levels of Y
-   return(mean(preds == trueY))
-
+  require(deepnet)
+  
+  if (scaleXMat) x <- scale(x)
+  
+  tmp <- splitData(x,nHoldout,idxsOnly=TRUE)
+  trainingx <- x[-tmp,]
+  testingx <- x[tmp,]
+  ym <- as.matrix(y)
+  trainingy <- ym[-tmp,]
+  testingy <- ym[tmp,]
+  
+  cmd <- paste0('nnout <- nn.train(trainingx,trainingy,')
+  cmd <- paste0(cmd,'hidden=',hidden,',') 
+  cmd <- paste0(cmd,'output=',output,',') 
+  cmd <- paste0(cmd,'numepochs=',numepochs) 
+  cmd <- paste0(cmd,')')
+  eval(parse(text=cmd))
+  preds <- nn.predict(nnout,testingx)
+  if (ncol(ym) == 1)  # regression case
+    return(mean(abs(preds - testingy)))
+  # else
+  preds <- apply(preds,1,which.max)  # column numbers
+  trueY <- apply(testingy,1,function(rw) which(rw == 1))
+  # convert to levels of Y
+  return(mean(preds == trueY))
+  
 }
 
 ######################  splitData() #################################
@@ -194,10 +225,10 @@ moveY <- function(xy,yCol)
 # unless xOnly is TRUE, in which case xy is just the X columns
 scaleX <- function(xy) 
 {  
-   ncxy <- ncol(xy)
-   x <- xy[,-ncxy]
-   x <- scale(x)
-   xy[,-ncxy] <- x
-   xy
+  ncxy <- ncol(xy)
+  x <- xy[,-ncxy]
+  x <- scale(x)
+  xy[,-ncxy] <- x
+  xy
 }
 
