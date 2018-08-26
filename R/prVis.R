7962612e649ba1336fba51210f841cf4b84da279
Norm Matloff <matloff@cs.ucdavis.edu>
2018-08-26 09:44:25 -0700

    more args

--- R/prVis.R
+++ R/prVis.R
@@ -18,32 +18,46 @@
 
 # arguments:
  
-#    xy:  data frame, Y in last column; factor means it's a classification
-#         problem
+#    xy:  data frame
+#    labels:  if TRUE, last column is Y for a classification problem;
+#             must be an R factor, unless nIntervals is non-NULL, in
+#             which case Y will be discretized to make labels
 #    deg:  degree of polynomial expansion
 #    nSubSam:  number of rows to randomly select; 0 means get all
 #    nIntervals: in regression case, number of intervals to use for
-#                partioning Y range
+#                partioning Y range to create labels
 #    saveOutputs: if TRUE, return list with gpOut = output of getPoly(), 
 #                  prout = output of prcomp()
 
-prVis <- function(xy,deg=2,nSubSam=2000,nIntervals=NULL,saveOutputs=FALSE)
-{ 
-  if (nSubSam > 0) xy <- xy[sample(1:nrow(xy),nSubSam),]
-  ydata <- xy[,ncol(xy)]
-  if (is.null(nIntervals) && !is.factor(ydata))
-     stop('Y must be a factor for classif.; set nIntervals for regress.')
-  if (!is.factor(ydata)) {
-    rng <- range(ydata)
-    increm <- (rng[2] - rng[1]) / nIntervals
-    ydata <- round((ydata - rng[1]) / increm)
-  }
+prVis <- function(xy,labels=FALSE,deg=2,nSubSam=2000,nIntervals=NULL,
+   saveOutputs=FALSE)
+{  
+  nrxy <- nrow(xy)
+  ncxy <- ncol(xy)
 
-  xdata <- xy[,-ncol(xy), drop=FALSE]
+  if (nSubSam < nrxy && nSubSam > 0)  
+     xy <- xy[sample(1:nrxy,nSubSam),]
+
+  if (labels) {
+     ydata <- xy[,ncxy]
+     if (is.null(nIntervals) && !is.factor(ydata))
+        stop('Y must be a factor for classif.; set nIntervals for regress.')
+     if (!is.null(nIntervals)) {
+       rng <- range(ydata)
+       increm <- (rng[2] - rng[1]) / nIntervals
+       ydata <- round((ydata - rng[1]) / increm)
+       ydata <- as.factor(ydata)
+     }
+     xdata <- xy[,-ncxy, drop=FALSE]
+  } else xdata <- xy
+
   xdata <- as.matrix(xdata)
   polyMat <- getPoly(xdata, deg)$xdata
   x.pca <- prcomp(polyMat,center=TRUE)
-  plot(x.pca$x[,1:2], col=ydata, pch=15, cex=0.5)
+  if (labels)  {
+     plot(x.pca$x[,1:2], col=ydata, pch=15, cex=0.5) 
+   } else plot(x.pca$x[,1:2], pch=15, cex=0.5)
+  
   if (saveOutputs) 
      return(list(gpOut=polyMat,prout=x.pca))
 }
