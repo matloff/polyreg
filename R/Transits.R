7966c4e43acb48ed6ae1d8b21274371982d08bdd
Norm Matloff <matloff@cs.ucdavis.edu>
2018-09-15 00:38:30 -0700

    needs work

--- R/Transits.R
+++ R/Transits.R
@@ -5,6 +5,7 @@
 # arguments:
 
 # img:  image data frame, one row per pixel row, class ID in last col
+# nr, nc:  numbers of rows and cols of pixels in one image
 # deg:  desired polynomial degree
 # maxBright: max pixel value 
  
@@ -12,41 +13,64 @@
  
 # S3 object of class 'transits', to be fed into predict.transits()
 
-transits <- function(img,deg,maxBright=255) 
+transits <- function(img,nr,nc,deg,maxFlips=10,maxBright=255) 
 {
-   nr <- nrow(img)
-   nc <- ncol(img)
-
    # convert to 0,1
    inc <- img[,-nc]
    inc <- round(inc / maxBright)
 
-   tmp <- apply(inc,1,getNewX)
+   ## works for small number of rows, not large; "sapply() ATTEMPTS to
+   ## create a matrix..."
+   ## inct <- as.data.frame(t(inc))
+   ## tmp <- sapply(inct,getNewX,nr,maxFlips)
+   inct <- as.data.frame(t(inc))
+   tmp <- lapply(inct,getNewX,nr,maxFlips)
+   browser()
+#       tmp <- NULL
+#       for (i in 1:nrow(inc)) {
+#          rw <- as.matrix(inc[i,])
+#          tmp <- cbind(tmp,getNewX(rw,nr,maxFlips))
+#       }
+   xformedImg <- cbind(t(tmp),img[,nc])
+   names(xformedImg)[nc] <- 'y'
 
-   xformedImg <- cbind(tmp,img)
+   # now need to do the regression
 
 }
 
-getNewX <- function(oneImg,nr) 
+getNewX <- function(oneImg,nr,maxFlips) 
 {
    # get transition counts
-   inc <- matrix(matrix(oneImg,byrow=TRUE,nrow=nr)
-   tmp <- getFlips(inc)
+   inc <- matrix(oneImg,byrow=TRUE,nrow=nr)
+   tmp <- getFlips(inc,maxFlips)
 
-   # form new X vector for this image 
-   tmph <- sapply(tmp$h,function(z) z)
-   tmpv <- sapply(tmp$v,function(z) z)
-   c(tmph,tmpv)
+   # form new X vector for this image
+   maxFlips1 <- maxFlips + 1
+   h <- rep(0,maxFlips1)
+   names(h) <- paste0('h',as.character(0:maxFlips))
+   h[names(tmp$h)] <- tmp$h
+   v <- rep(0,maxFlips1)
+   names(v) <- paste0('v',as.character(0:maxFlips))
+   v[names(tmp$v)] <- tmp$v
+   c(h,v)
 }
 
 # finds the transition counts for this image, 'inc'; returns an R list,
 # one element per number of flips found (horiz. or vert.)
 
-getFlips <- function(inc) 
+getFlips <- function(inc,maxFlips) 
 {
    flips <- function(x) x[-length(x)]!= x[-1]
    flipCount <- function(x) sum(flips(x))
-   horiz <- apply(inc,1,flipCount)
-   vert <- apply(inc,2,flipCount)
-   list(h = as.list(table(horiz)), v = as.list(table(vert)))
+   horiz <- apply(inc,1,flipCount) / 2
+   th <- table(horiz)
+   h <- as.vector(th)
+   names(h) <- names(th) 
+   names(h) <- paste0('h',names(h))
+   vert <- apply(inc,2,flipCount) / 2
+   tv <- table(vert)
+   v <- as.vector(tv)
+   names(v) <- names(tv) 
+   names(v) <- paste0('v',names(v))
+   list(h = h, v = v)
 }
