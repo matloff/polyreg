f9371c722a09e503eff4d31b010697ff57267c55
Pete Mohanty <pete.mohanty@gmail.com>
2019-03-22 11:59:47 -0700

    remove toDummies()

--- R/misc.R
+++ R/misc.R
@@ -13,21 +13,3 @@ toFactors <- function(df,cols)
    df
 }
 
-# converts a single column to a matrix of dummy variables; column names
-# will be concatenation of the original column names and levels(the
-# column); column must be a factor
-# SHOULD NOT BE USED with getPoly() or FSR()
-#' @export
-toDummies <- function(df,col)
-{
-   # require(dummies)
-   # requireNamespace(dummies)
-   dumms <- dummies::dummy(df[,col])
-   namesDumms <- paste0(names(df)[col],levels(df[,col]))
-   tmp <- cbind(df[,-col],dumms)
-   nctmp <- ncol(tmp)
-   ncdumms <- ncol(dumms)
-   names(tmp)[(nctmp-ncdumms+1):nctmp] <- namesDumms
-   tmp
-}
-
