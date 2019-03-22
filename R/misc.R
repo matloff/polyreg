00bef6cea80f10f2690a5e5292713a30bb328a27
Pete Mohanty <pete.mohanty@gmail.com>
2019-03-22 10:16:01 -0700

    update docs

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,10 @@
 
 # converts each df column in cols to factor, returns new df
-
+# for getPoly(), and by extension polyFit() and FSR():
+# should be used on any categorical variable stored as an integer
+# is optional for binary variables
+# is optional for categorical variables stored as characters
+#' @export
 toFactors <- function(df,cols)
 {  
    for (i in cols) {
@@ -12,7 +16,8 @@ toFactors <- function(df,cols)
 # converts a single column to a matrix of dummy variables; column names
 # will be concatenation of the original column names and levels(the
 # column); column must be a factor
-
+# SHOULD NOT BE USED with getPoly() or FSR()
+#' @export
 toDummies <- function(df,col)
 {
    # require(dummies)
